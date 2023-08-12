c6a30be09445802e84a0011be34025e3473d505d
Neoh Chen Lun <neohchenlun@gmail.com>
2023-08-13 01:43:45 +0800

    finalized login system

--- Assignment/source.c
+++ Assignment/source.c
@@ -3,8 +3,9 @@
 #include <math.h>
 #include <string.h>
 #pragma warning (disable:4996)
-int menuSelect,loginSelect,pass1,pass2,pass3,line,linecheck,AIDline=1,SIDline=1;
-char staffID[10], staffPW[20];
+int menuSelect,loginSelect,pass1,pass2,pass3,accID,linecheck,SIDline;
+char staffID[10], staffPW[20],PIN[255];
+char* PINcheck[] = {"234567","123456","234565"};
 int menu() {
 	for (int i = 0; i < 50; i++) {
 		putchar('=');
@@ -21,43 +22,37 @@ int menu() {
 	return menuSelect;
 }
 int getAID() {
-	char AID[255],c;
+	char AID[255], c, IDcheck[10];
 	printf("Enter login credentials\nAdmin ID:");
-	scanf("%s",AID);
+	scanf("%s", AID);
 	rewind(stdin);
-	FILE *file1 = fopen("AID.txt", "r");
-	char IDcheck[10];
-	for (c = getc(file1);c != EOF; c = getc(file1)) {
-		if (c == '\n') // Increment count if this character is newline
-			AIDline++;
-	}
+	FILE* file1 = fopen("AID.txt", "r");
 	rewind(file1);
 	while (fgets(IDcheck, sizeof(IDcheck), file1)) {
 		if (strcmp(AID, IDcheck) == 0) {
-			line=(line/2)+1;
+			accID/=2;
 			printf("Logged in successfully.\n");
 			pass1 = 1;//is a global variable set to 0;allows the loop to exit
-			fclose(file1);
-			break;
+			fclose(file1);	
 		}
-		else { line++; }
+		else {accID++;}
 	}
-	return pass1,line;
+	return pass1,accID;
 }
 int getSID(){
-	char SID[255],c;
+	char SID[255],c,IDcheck[10];
 	printf("Enter login credentials\nStudent ID:");
 	scanf("%s",SID);
 	rewind(stdin);
-	FILE *file2 = fopen("SID.txt", "r");
-	char IDcheck2[10];
+	FILE *file2 = fopen("SID.txt","r");
 	for (c = getc(file2);c != EOF; c = getc(file2)) {
 		if (c == '\n') // Increment count if this character is newline
 			SIDline++;
 	}
-	while (fgets(IDcheck2, sizeof(IDcheck2), file2)) {
-		if (strcmp(SID,IDcheck2)==0) {
-			printf("Logged in successfully.\n%d",SIDline);
+	rewind(file2);
+	while (fgets(IDcheck, sizeof(IDcheck), file2)) {
+		if (strcmp(SID,IDcheck)==0) {
+			printf("Logged in successfully.\n");
 			pass3 = 1;//is a global variable set to 0;allows the loop to exit
 			fclose(file2);
 			break;
@@ -80,6 +75,17 @@ int login() {
 	}
 	return loginSelect;
 }
+int getPW(accID) {
+	printf("Enter PIN:");
+	scanf("%s", PIN);
+	if (strcmp(PIN,PINcheck[accID])==0) {
+		rewind(stdin);
+		printf("Access granted\n");
+		pass2 = 1;
+	}
+	else {printf("Wrong password\n");}
+	return pass2;
+}
 void main() {
 	int loop = 1;
 	while (loop == 1) {
@@ -88,13 +94,21 @@ void main() {
 			login();
 			if (loginSelect == 1) {
 				pass1 = 0, pass2 = 0;
-				while (pass1 == 0) { line = 1;getAID(); }
-				while (pass1 == 1 && pass2 == 0) {}
-				while (pass1 == 1 && pass2 == 1) { printf("test"); }
+				while (pass1 == 0) { 
+					accID = 1;
+					getAID();
+				}
+				while (pass1 == 1 && pass2 == 0) {
+					getPW(accID);
+				}
+				while (pass1 == 1 && pass2 == 1) { printf("test");break; }
 			}
 			if (loginSelect == 2) {
 				pass3 = 0;
-				while (pass3 == 0) { getSID(); }
+				while (pass3 == 0) {
+					SIDline = 1;
+					getSID();
+				}
 			}
 		}
 		else if (menuSelect==2){}
