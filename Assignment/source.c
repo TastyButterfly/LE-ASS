76169e9185ce0f98e411fd428b24f8fd42bf795f
Neoh Chen Lun <neohchenlun@gmail.com>
2023-08-31 03:17:56 +0800

    added confirmation system; fixed grading system

--- Assignment/source.c
+++ Assignment/source.c
@@ -4,7 +4,7 @@
 #include <string.h>
 #pragma warning (disable:4996)
 int menuSelect,loginSelect,pass1,pass2,pass3,accID,linecheck,SIDline,attempt;
-char staffID[10], staffPW[20], PIN[255], studentDetails[][5][60] = { {"KPKL1234","idk","20","July","November"},{""} };studSem1Sub[][13][8] = { {"AAA1003","AAA1014","AAA1013","AAA1024","AAA1024","AAA1052"}}, studSem2Sub[][13][8]={ {"AAA1103","AAA1114","AAA1113","AAA1124","AAA1124","AAA1152"} }, studSem3Sub[][13][8]= { {"AAA1203","AAA1214","AAA1213","AAA1224","AAA1224","AAA1252"} };
+char PIN[8], studentDetails[][5][60] = { {"KPKL1234","idk","20","July","November"},{""} },studSem1Sub[][13][8] = { {"AAA1003","AAA1014","AAA1013","AAA1024","AAA1024","AAA1052"}}, studSem2Sub[][13][8]={ {"AAA1103","AAA1114","AAA1113","AAA1124","AAA1124","AAA1152"} }, studSem3Sub[][13][8]= { {"AAA1203","AAA1214","AAA1213","AAA1224","AAA1224","AAA1252"} };
 char* PINcheck[] = {"234567","123456","234565"};
 float studSem1GPA[][7]={{4.0,4.0,4.0,4.0,4.0,4.0,4.0}}, studSem2GPA[][7] = { {4.0,4.0,4.0,4.0,4.0,4.0,4.0} }, studSem3GPA[][7] = { {4.0,4.0,4.0,4.0,4.0,4.0,4.0} };
 int menu() {
@@ -26,12 +26,12 @@ int menu() {
 	return menuSelect;
 }
 int getAID() {
-	char AID[255], c, IDcheck[10];
+	char AID[11], IDcheck[10];
 	if (attempt == 1) {
 		printf("Enter login credentials\nAdmin ID:");
 	}
 	else { printf("Record not found!\nAdmin ID:"); }
-	scanf("%s", AID);
+	scanf("%10s", AID);
 	rewind(stdin);
 	FILE* file1 = fopen("AID.txt", "r");
 	rewind(file1);
@@ -49,19 +49,23 @@ int getAID() {
 	return pass1,accID,attempt;
 }
 char getSID() {
-	char SID[255];
+	char SID[9];
 	int IDcheck=0,IDcheckline = 0;
 	if (attempt == 1) {
-		printf("Enter login credentials\nStudent ID:");
+		printf("Enter login credentials(0 to exit)\nStudent ID:");
 	}
-	else { printf("Record not found!\nStudent ID:"); }
-	scanf("%s",SID);
+	else { printf("Record not found!(0 to exit)\nStudent ID:"); }
+	scanf("%9s",SID);
 	rewind(stdin);
 	for (int i = 0;i <(sizeof(studentDetails)/sizeof(studentDetails[0])); i++) {
 		if (strcmp(SID,studentDetails[i][0]) == 0) {
 			printf("Logged in successfully.\nWelcome, %s",studentDetails[i][1]);
 			pass3 = 1;//is a global variable set to 0;allows the loop to exit
 			attempt = 1;
+			break;
+		}
+		else if(strcmp(SID, "0") == 0) {
+			pass3 = -1;
 			break;
 		}
 	}
@@ -86,7 +90,7 @@ int login() {
 }
 int getPW() {
 	printf("Enter PIN:");
-	scanf("%s", PIN);
+	scanf("%7s", PIN);
 	if (strcmp(PIN,PINcheck[accID])==0) {
 		rewind(stdin);
 		printf("Access granted\n");
@@ -105,30 +109,81 @@ char adminMenu() {
 		putchar('=');
 	}
 	//above is to print admin menu, clears console
-	int select, subSelect, i, loop = 1, loop2 = 1, menu2select, subjectLoop, ID, semSelect, gradeLoop = 1;
-	char IDcheck[10], grade[2];
+	int select, subSelect, i, loop = 1, loop2 = 1, menu2select, subjectLoop, ID,check, semSelect, gradeLoop = 1;
+	char IDcheck[10], grade[2],stdID[10],IDConfirm[10],stdName[60],nameConfirm[60];
 	while (loop == 1) {
-		printf("\n1.Add new student\n2.Enter student course details\n3.View students' CGPA and GPA score.\n");//prints menu
+		printf("\n1.Add new student.\n2.Enter student course details.\n3.View students' CGPA and GPA score.\n0.Exit\n");//prints menu
 		while (scanf("%d", &select), select < 1 || select >3) {
 			rewind(stdin);
 			printf("Enter a value between 1 to 3!");
 		}//checks input
 		switch (select) {
+		case 0:loop = 0;
 		case 1:
 			system("cls");
 			for (ID = 0; ID < sizeof(studentDetails) / sizeof(studentDetails[0]);ID++) {
 				if (strcmp(studentDetails[ID][0], "") == 0) { break; }
 			}
 			printf("Enter new student ID:");
-			scanf("%s", studentDetails[ID][0]);
+			scanf("%9s", studentDetails[ID][0]);
 			rewind(stdin);
+			printf("Confirm ID (ID cannot be changed!):");
+			scanf("%9s", IDConfirm);
+			rewind(stdin);
+			for (check= 0; check <(sizeof(studentDetails) / sizeof(studentDetails[0]))-1;check++) {
+				if (strcmp(studentDetails[ID][0],studentDetails[check][0]) == 0) {
+						printf("ID does not match, is invalid or is already used!\n");
+						printf("Enter new student ID:");
+						scanf("%8s", studentDetails[ID][0]);
+						rewind(stdin);
+						printf("Confirm ID (ID cannot be changed!):");
+						scanf("%9s", IDConfirm);
+						rewind(stdin);
+				}
+			}
+			while (strcmp(studentDetails[ID][0], IDConfirm) != 0) {
+				printf("ID does not match, is invalid or is already used!\n");
+				printf("Enter new student ID:");
+				scanf("%8s", studentDetails[ID][0]);
+				rewind(stdin);
+				printf("Confirm ID (ID cannot be changed!):");
+				scanf("%9s", IDConfirm);
+				rewind(stdin);
+			}
 			strcpy(studSem1Sub[ID][0],studentDetails[ID][0]);
 			strcpy(studSem2Sub[ID][0], studentDetails[ID][0]);
 			strcpy(studSem3Sub[ID][0], studentDetails[ID][0]);
 			strcpy(studentDetails[ID + 1][0], "");
-			printf("Enter student name:");
-			scanf("%[^\n]", studentDetails[ID][1]);
+			printf("Enter new student name:");
+			scanf("%59[^\n]", studentDetails[ID][1]);
 			rewind(stdin);
+			printf("Confirm name (name cannot be changed!):");
+			scanf("%59[^\n]", nameConfirm);
+			rewind(stdin);
+			for (check = 0; check < (sizeof(studentDetails) / sizeof(studentDetails[0])) - 1;check++) {
+				if (strcmp(studentDetails[ID][1], studentDetails[check][1]) == 0) {
+					printf("Name does not match or is already used!\n");
+					printf("Enter new student name:");
+					scanf("%59[^\n]", studentDetails[ID][1]);
+					rewind(stdin);
+					printf("Confirm name (name cannot be changed!):");
+					scanf("%59[^\n]", nameConfirm);
+					rewind(stdin);
+				}
+			}
+			while (strcmp(studentDetails[ID][1], nameConfirm) != 0) {
+				for (check = 0; check <= sizeof(studentDetails) / sizeof(studentDetails[0]);check++) {
+					if (strcmp(studentDetails[ID][1], studentDetails[check][1]) == 0) {
+						printf("Name does not match or is already used!\n");
+						printf("Enter new student name:");
+						scanf("%59[^\n]", studentDetails[ID][1]);
+						rewind(stdin);
+						printf("Confirm name (name cannot be changed!):");
+						scanf("%59[^\n]", nameConfirm);
+						rewind(stdin);
+					}
+				}
+			}
 			printf("Would you like to continue editing student details? Enter 1 to continue, any other value to stop.");
 			scanf("%d", &loop);
 			break;
@@ -180,35 +235,35 @@ char adminMenu() {
 							printf("Enter grade obtained:");
 							scanf("%2s", grade);
 							while (gradeLoop == 1) {
-								if (strcmp(grade, "A")) {
+								if (strcmp(grade, "A")==0) {
 									studSem1GPA[ID][n] = 4.0;
 									gradeLoop = 0;
 								}
-								else if (strcmp(grade, "A-")) {
+								else if (strcmp(grade, "A-")==0) {
 									studSem1GPA[ID][n] = 3.75;
 									gradeLoop = 0;
 								}
-								else if (strcmp(grade, "B+")) {
+								else if (strcmp(grade, "B+")==0) {
 									studSem1GPA[ID][n] = 3.50;
 									gradeLoop = 0;
 								}
-								else if (strcmp(grade, "B")) {
+								else if (strcmp(grade, "B")==0) {
 									studSem1GPA[ID][n] = 3.00;
 									gradeLoop = 0;
 								}
-								else if (strcmp(grade, "B-")) {
+								else if (strcmp(grade, "B-")==0) {
 									studSem1GPA[ID][n] = 2.75;
 									gradeLoop = 0;
 								}
-								else if (strcmp(grade, "C+")) {
+								else if (strcmp(grade, "C+")==0) {
 									studSem1GPA[ID][n] = 2.50;
 									gradeLoop = 0;
 								}
-								else if (strcmp(grade, "C")) {
+								else if (strcmp(grade, "C")==0) {
 									studSem1GPA[ID][n] = 2.00;
 									gradeLoop = 0;
 								}
-								else if (strcmp(grade, "F")) {
+								else if (strcmp(grade, "F")==0) {
 									studSem1GPA[ID][n] = 0.00;
 									gradeLoop = 0;
 								}
@@ -232,35 +287,35 @@ char adminMenu() {
 							printf("Enter grade obtained:");
 							scanf("%2s", grade);
 							while (gradeLoop == 1) {
-								if (strcmp(grade, "A")) {
+								if (strcmp(grade, "A")==0) {
 									studSem2GPA[ID][n] = 4.0;
 									gradeLoop = 0;
 								}
-								else if (strcmp(grade, "A-")) {
+								else if (strcmp(grade, "A-")==0) {
 									studSem2GPA[ID][n] = 3.75;
 									gradeLoop = 0;
 								}
-								else if (strcmp(grade, "B+")) {
+								else if (strcmp(grade, "B+")==0) {
 									studSem2GPA[ID][n] = 3.50;
 									gradeLoop = 0;
 								}
-								else if (strcmp(grade, "B")) {
+								else if (strcmp(grade, "B")==0) {
 									studSem2GPA[ID][n] = 3.00;
 									gradeLoop = 0;
 								}
-								else if (strcmp(grade, "B-")) {
+								else if (strcmp(grade, "B-")==0) {
 									studSem2GPA[ID][n] = 2.75;
 									gradeLoop = 0;
 								}
-								else if (strcmp(grade, "C+")) {
+								else if (strcmp(grade, "C+")==0) {
 									studSem2GPA[ID][n] = 2.50;
 									gradeLoop = 0;
 								}
-								else if (strcmp(grade, "C")) {
+								else if (strcmp(grade, "C")==0) {
 									studSem2GPA[ID][n] = 2.00;
 									gradeLoop = 0;
 								}
-								else if (strcmp(grade, "F")) {
+								else if (strcmp(grade, "F")==0) {
 									studSem2GPA[ID][n] = 0.00;
 									gradeLoop = 0;
 								}
@@ -284,35 +339,35 @@ char adminMenu() {
 							printf("Enter grade obtained:");
 							scanf("%2s", grade);
 							while (gradeLoop == 1) {
-								if (strcmp(grade, "A")) {
+								if (strcmp(grade, "A")==0) {
 									studSem3GPA[ID][n] = 4.0;
 									gradeLoop = 0;
 								}
-								else if (strcmp(grade, "A-")) {
+								else if (strcmp(grade, "A-")==0) {
 									studSem3GPA[ID][n] = 3.75;
 									gradeLoop = 0;
 								}
-								else if (strcmp(grade, "B+")) {
+								else if (strcmp(grade, "B+")==0) {
 									studSem3GPA[ID][n] = 3.50;
 									gradeLoop = 0;
 								}
-								else if (strcmp(grade, "B")) {
+								else if (strcmp(grade, "B")==0) {
 									studSem3GPA[ID][n] = 3.00;
 									gradeLoop = 0;
 								}
-								else if (strcmp(grade, "B-")) {
+								else if (strcmp(grade, "B-")==0) {
 									studSem3GPA[ID][n] = 2.75;
 									gradeLoop = 0;
 								}
-								else if (strcmp(grade, "C+")) {
-									studSem1GPA[ID][n] = 2.50;
+								else if (strcmp(grade, "C+")==0) {
+									studSem3GPA[ID][n] = 2.50;
 									gradeLoop = 0;
 								}
-								else if (strcmp(grade, "C")) {
+								else if (strcmp(grade, "C")==0) {
 									studSem3GPA[ID][n] = 2.00;
 									gradeLoop = 0;
 								}
-								else if (strcmp(grade, "F")) {
+								else if (strcmp(grade, "F")==0) {
 									studSem3GPA[ID][n] = 0.00;
 									gradeLoop = 0;
 								}
@@ -346,8 +401,8 @@ char adminMenu() {
 }
 	
 void main(){
-	int loop = 1;
-	while (loop == 1) {
+	char loop = '1';
+	while (loop == '1') {
 		attempt = 1;
 		menu();
 		if (menuSelect == 1) {
@@ -373,7 +428,8 @@ void main(){
 		}
 		else if (menuSelect==2){}
 		printf("\nWould you like to continue? Type 1 to continue; any other value to exit.\n");
-		scanf("%d", &loop);
+		scanf("%c", &loop);
+		rewind(stdin);
 		system("cls");//clears console
 	}
 }
